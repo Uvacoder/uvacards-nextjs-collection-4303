6161de9bf0dbd2efa50f7e623b7f332cfb2a0098
uvacoder <uva22940coder@gmail.com>
2022-11-18 14:16:17 -0500

    Update bio.tsx

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -2,22 +2,22 @@ import {Dribbble, Email, Twitter, GitHub} from './icons'
 
 const links = [
   {
-    href: 'https://twitter.com/vjthlk',
+    href: 'https://twitter.com/VaLandscapes',
     icon: <Twitter />,
     label: 'Twitter',
   },
   {
-    href: 'https://github.com/vojtaholik',
+    href: 'https://github.com/uvacoder',
     icon: <GitHub />,
     label: 'GitHub',
   },
   {
-    href: 'https://dribbble.com/vjthlk',
+    href: 'https://dribbble.com/uvacoder',
     icon: <Dribbble />,
     label: 'Dribbble',
   },
   {
-    href: 'mailto:vojta@holik.dev',
+    href: 'mailto:uvacoder@proton.me',
     icon: <Email />,
     label: 'Email',
   },
